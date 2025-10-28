5e21068dd277455a9d5d1e4dda310999f9d23063
[dyad] <estevao.v.garcia10@gmail.com>
2025-10-28 15:42:33 -0300

    [dyad] Corrigi o erro 'React.Children.only' filtrando itens nulos da lista de navegação na sidebar. - wrote 1 file(s)

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -124,7 +124,7 @@ export function AppSidebar() {
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 );
-              })}
+              }).filter(Boolean)} {/* Adicionado .filter(Boolean) aqui */}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
