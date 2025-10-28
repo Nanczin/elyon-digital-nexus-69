45caae2c967e8201288a9a5c303e520f25345e70
[dyad] <estevao.v.garcia10@gmail.com>
2025-10-28 15:33:38 -0300

    [dyad] Ajustei o padding dos itens da sidebar para que os ícones comecem na borda esquerda. - wrote 1 file(s)

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -113,7 +113,7 @@ export function AppSidebar() {
                         to={item.href} 
                         className={({ isActive }) => 
                           `${getNavCls({ isActive })} flex items-center rounded-md ${
-                            isCollapsed ? 'justify-center px-0' : 'gap-3 px-3' // Alterado de 'pl-0 pr-4' para 'px-3'
+                            isCollapsed ? 'justify-center px-0' : 'gap-3 pl-0 pr-3' // Alterado de 'px-3' para 'pl-0 pr-3'
                           }`
                         }
                         title={isCollapsed ? item.label : undefined}
