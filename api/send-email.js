2929930d34bae9f2773960bfa0d1b3f4a611cdd1
[dyad] <estevao.v.garcia10@gmail.com>
2025-10-29 22:23:26 -0300

    [dyad] Converti a função `api/send-email.js` para ES Module e pedi para o usuário reconstruir a aplicação. - wrote 1 file(s)

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+import nodemailer from 'nodemailer';
 
-module.exports = async (req, res) => {
+export default async function (req, res) {
   // Configurar cabeçalhos CORS para permitir requisições das Edge Functions do Supabase
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -67,4 +67,4 @@ module.exports = async (req, res) => {
     // Retornar um status 500 para indicar falha à Edge Function que chamou
     res.status(500).json({ success: false, error: 'Falha ao enviar e-mail.', details: error.message, nodemailerError: error.response || error.message });
   }
-};
+}
