213918ee7b481016789bad82f45dc2edefe8ff6c
[dyad] <estevao.v.garcia10@gmail.com>
2025-11-03 17:45:33 -0300

    [dyad] Corrigi o erro de sintaxe na importação de `ManageMemberAccessDialog` em `ProjectMembersPage.tsx`. - wrote 1 file(s)

--- src/pages/project-management/ProjectMembersPage.tsx
+++ src/pages/project-management/ProjectMembersPage.tsx
@@ -12,7 +12,7 @@ import { Badge } from '@/components/ui/badge';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { NewMemberDialog } from '@/components/elyon-builder/NewMemberDialog';
-import { ManageMemberAccessDialog } '@/components/elyon-builder/ManageMemberAccessDialog';
+import { ManageMemberAccessDialog } from '@/components/elyon-builder/ManageMemberAccessDialog';
 import { Switch } from '@/components/ui/switch';
 
 interface Project {
